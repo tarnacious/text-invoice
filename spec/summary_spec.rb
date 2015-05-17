d737c6752caa8339f4b8ecf3d5a9bcf3bb17a6ae
Guillaume Virlet <github@virlet.org>
2015-05-17 11:01:25 +0200

    Convert specs to RSpec 3.2.3 syntax with Transpec
    This conversion is done by Transpec 3.1.0 with the following command:
        transpec -f
    
    * 36 conversions
        from: obj.should
          to: expect(obj).to
    
    * 28 conversions
        from: obj.should_receive(:message)
          to: expect(obj).to receive(:message)
    
    * 2 conversions
        from: obj.stub(:message)
          to: allow(obj).to receive(:message)
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/summary_spec.rb
+++ spec/summary_spec.rb
@@ -8,32 +8,32 @@ describe TextInvoice::Summary do
         invoice = summary.load("lib/text-invoice/invoice.yaml").to_yaml
 
         blank = calculate.process(TextInvoice::Invoice.blank)
-        invoice.should eql(blank) 
+        expect(invoice).to eql(blank) 
     end
 
     it "should return invoice summary" do
         summary = TextInvoice::Summary.new
         invoice1 = { 'due' => 5, 'total' => 10, 'paid' => 5 } 
         invoice2 = { 'due' => 1, 'total' => 10, 'paid' => 9 } 
-        summary.stub(:load).and_return(invoice1, invoice2)
+        allow(summary).to receive(:load).and_return(invoice1, invoice2)
         
         response = summary.summary(['file1','file2'])
     
-        headings = response.split("\n")[0].should eql(['Count','Total','Paid','Due'].join("\t"))
-        results = response.split("\n")[1].should eql(['2','20','14','6'].join("\t"))
+        headings = expect(response.split("\n")[0]).to eql(['Count','Total','Paid','Due'].join("\t"))
+        results = expect(response.split("\n")[1]).to eql(['2','20','14','6'].join("\t"))
     end
     
     it "should return invoice list" do
         summary = TextInvoice::Summary.new
         invoice1 = { 'date' => '1/3/2012', 'invoice' => "1", 'due' => 5, 'total' => 10, 'paid' => 5 } 
         invoice2 = { 'date' => '2/3/2012', 'invoice' => "2", 'due' => 1, 'total' => 10, 'paid' => 9 } 
-        summary.stub(:load).and_return(invoice1, invoice2)
+        allow(summary).to receive(:load).and_return(invoice1, invoice2)
 
         response = summary.list(['file1','file2'])
     
-        response.split("\n")[0].should eql(['Invoice','Date','Total','Paid','Due'].join("\t"))
-        response.split("\n")[1].should eql(['1','1/3/2012','10','5','5'].join("\t"))
-        response.split("\n")[2].should eql(['2','2/3/2012','10','9','1'].join("\t"))
+        expect(response.split("\n")[0]).to eql(['Invoice','Date','Total','Paid','Due'].join("\t"))
+        expect(response.split("\n")[1]).to eql(['1','1/3/2012','10','5','5'].join("\t"))
+        expect(response.split("\n")[2]).to eql(['2','2/3/2012','10','9','1'].join("\t"))
     end
 end
 
--- spec/totals_spec.rb
+++ spec/totals_spec.rb
@@ -10,8 +10,8 @@ describe TextInvoice::Totals do
         result = YAML.load(calculator.process(invoice))
         
         # validate
-        result["total"].should eql(0)
-        result["due"].should eql(0)
+        expect(result["total"]).to eql(0)
+        expect(result["due"]).to eql(0)
     end
     
     it "should update invoice totals and subtotals" do
@@ -26,10 +26,10 @@ describe TextInvoice::Totals do
         result = YAML.load(calculator.process(invoice))
 
         # validate
-        result["items"][0]["subtotal"].should eql(4)
-        result["items"][1]["subtotal"].should eql(6)
-        result["total"].should eql(10)
-        result["due"].should eql(10)
+        expect(result["items"][0]["subtotal"]).to eql(4)
+        expect(result["items"][1]["subtotal"]).to eql(6)
+        expect(result["total"]).to eql(10)
+        expect(result["due"]).to eql(10)
     end
     
     it "should update 'due' amount to be 'total' minus 'paid'" do
@@ -42,8 +42,8 @@ describe TextInvoice::Totals do
         result = YAML.load(calculator.process(invoice))
 
         # validate
-        result["total"].should eql(10)
-        result["paid"].should eql(5)
-        result["due"].should eql(5)
+        expect(result["total"]).to eql(10)
+        expect(result["paid"]).to eql(5)
+        expect(result["due"]).to eql(5)
     end
 end
