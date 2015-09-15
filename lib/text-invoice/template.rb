d36c4b96b2cfe21e977e913e5d927e879af4e56e
tarnacious <tarn@tarnbarford.net>
2015-09-15 11:25:17 +0200

    Calculate subtotals / totals

--- lib/text-invoice/template.rb
+++ lib/text-invoice/template.rb
@@ -4,16 +4,18 @@ require 'mustache'
 module TextInvoice
     class Template
         def html(invoice)
-            parsed = YAML.load(invoice)
+            totals = TextInvoice::Totals.new
+            parsed = YAML.load(totals.process(invoice))
             Mustache.render(open(root + "templates/invoice.html").read(), parsed)
         end
-        
+
         def custom(invoice, template)
-            parsed = YAML.load(invoice)
+            totals = TextInvoice::Totals.new
+            parsed = YAML.load(totals.process(invoice))
             Mustache.render(open(template).read(), parsed)
         end
-        
-        def root 
+
+        def root
           File.dirname(__FILE__) + "/../../"
         end
     end
