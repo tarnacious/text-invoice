9e5028f3da97c62e45626299258a5a141136016b
tarnacious <tarn@tarnbarford.net>
2019-09-26 19:16:38 +0200

    Rounding fixes

--- lib/text-invoice/totals.rb
+++ lib/text-invoice/totals.rb
@@ -24,12 +24,21 @@ module TextInvoice
 
             parsed["subtotal"] = total
             parsed["tax"] = tax
-            parsed["total"] = total2
+            parsed["total"] = total2.round(2)
 
             if parsed["paid"]
                 parsed["due"] = (total2 - parsed["paid"]).round(2)
             else
                 parsed["due"] = total2
+            end
+
+            # convert to strings
+            parsed["subtotal_str"] = format("%.2f", parsed["subtotal"])
+            parsed["tax_str"] = format("%.2f", parsed["tax"])
+            parsed["total_str"] = format("%.2f", parsed["total"])
+            if parsed["paid"]
+                parsed["paid_str"] = format("%.2f", parsed["paid"])
+                parsed["due_str"] = format("%.2f", parsed["due"])
             end
 
             parsed.to_yaml
